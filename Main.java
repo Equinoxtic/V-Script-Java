3400e99d5c2c3eedbfd367235884a0a66a4ec4de
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-11 21:53:31 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import Classes.States.*;
+import Classes.Utility.*;
 import java.util.Scanner;
 
 public class Main
@@ -7,16 +8,22 @@ public class Main
 	{
 		Scanner input = new Scanner(System.in);
 		InputOutSelectionState IoSelect = new InputOutSelectionState();
+		CalculatorOptions CalcOptions = new CalculatorOptions();
+		Out wrln = new Out();
 
 		var curAction = 0;
 
-		System.out.print("Select an action:\n[1] Basic Input\n");
+		wrln.lnStr("Select an action:\n[1] Basic Input | [2] Calculator\n");
 		curAction = input.nextInt();
 
 		switch (curAction)
 		{
 			case 1:
 				IoSelect.IOSelection();
+				break;
+
+			case 2:
+				CalcOptions.CalcOpts();
 				break;
 		}
 		input.close();
