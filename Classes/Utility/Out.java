1e81ccbd941455c1efd7ce3801d089286c800a91
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-11 21:55:16 +0800

    lil' change

--- Classes/Utility/Out.java
+++ Classes/Utility/Out.java
@@ -3,15 +3,15 @@ package Classes.Utility;
 public class Out
 {
 	public void lnStr(String str) {
-		System.out.println(str);
+		System.out.print(str);
 	}
 	public void lnInt(int curInt) {
-		System.out.println(curInt);
+		System.out.print(curInt);
 	}
 	public void lnFloat(float curFloat) {
-		System.out.println(curFloat);
+		System.out.print(curFloat);
 	}
 	public void lnDouble(double curDouble) {
-		System.out.println(curDouble);
+		System.out.print(curDouble);
 	}
 }
